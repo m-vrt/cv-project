5b25bfcf00e19cd0d48bee6e5678d598f62d6681
m-vrt <160310122+m-vrt@users.noreply.github.com>
2025-02-12 17:48:15 +0800

    005 - Enabled CUDA

--- process_video.py
+++ process_video.py
@@ -6,6 +6,10 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
+import warnings
+
+
+warnings.filterwarnings("ignore", category=UserWarning, module="torch.functional")
 
 
 video_path = "for cv-project.MOV"
@@ -23,22 +27,22 @@ frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 
 resize_width, resize_height = 640, 360
-
-
-frame_skip = max(1, fps // 10) 
+frame_skip = max(1, fps // 10)  
 
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_50_FPN_3x.yaml"))
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/retinanet_R_50_FPN_3x.yaml")
 cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.4  
-cfg.MODEL.DEVICE = "cpu"  
+cfg.MODEL.DEVICE = "cuda"  
+cfg.MODEL.PIXEL_MEAN = [0.0, 0.0, 0.0]  
+cfg.MODEL.PIXEL_STD = [1.0, 1.0, 1.0]
 
 predictor = DefaultPredictor(cfg)
 
 
 output_path = "output_video.mp4"
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_path, fourcc, fps // frame_skip, (resize_width, resize_height))
 
 frame_count = 0
@@ -51,14 +55,14 @@ while cap.isOpened():
 
     frame_count += 1
 
-   
+    
     if frame_count % frame_skip != 0:
         continue
 
     
     frame = cv2.resize(frame, (resize_width, resize_height))
 
-   
+    
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
    
@@ -68,16 +72,16 @@ while cap.isOpened():
     v = Visualizer(frame_rgb, MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=0.8)
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
-    
+  
     processed_frame = cv2.cvtColor(v.get_image(), cv2.COLOR_RGB2BGR)
 
-   
+  
     out.write(processed_frame)
 
-
+   
     cv2.imshow("Object Detection", processed_frame)
 
-   
+  
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
